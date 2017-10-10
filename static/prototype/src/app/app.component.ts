3bf0f9be90c2d8b3d9da61372e6e6a75cc44d82d
Letty <git@letty.io>
2017-10-10 12:50:09 +0200

    add observer variables for selection service and api requests

--- static/prototype/src/app/app.component.ts
+++ static/prototype/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IYear, IPerson, ITopic} from "./app.interfaces";
+import {SelectionService} from "./services/selection.service";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private selectedYear: IYear;
+  private selectedPerson: IPerson;
+  private selectedTopic: ITopic;
+
+  constructor(private selection: SelectionService) {
+    selection.selPerson$.subscribe(
+      person => {
+        this.selectedPerson = person;
+      }
+    );
+
+    selection.selTopic$.subscribe(
+      topic => {
+        this.selectedTopic = topic;
+      }
+    );
+
+    selection.selYear$.subscribe(
+      year => {
+        this.selectedYear = year;
+      }
+    )
+  }
 }
