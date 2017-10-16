50c2be82f8ad9b7eec41e96efeb92d5a01a534b1
Letty <git@letty.io>
2017-10-16 18:12:26 +0200

    add dataservice

--- static/prototype/src/app/app.module.ts
+++ static/prototype/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TimelineComponent} from './components/timeline.component';
-import { PersonComponent} from './components/person.component';
-import { TopicComponent} from './components/topic.component';
-import { ApiService} from './services/api.service';
-import { SelectionService} from './services/selection.service';
-
+import {AppComponent} from './app.component';
+import {TimelineComponent} from './components/timeline.component';
+import {PersonComponent} from './components/person.component';
+import {TopicComponent} from './components/topic.component';
+import {ApiService} from './services/api.service';
+import {SelectionService} from './services/selection.service';
+import {DataService} from "./services/data.service";
 
 
 @NgModule({
@@ -24,10 +24,12 @@ import { SelectionService} from './services/selection.service';
   ],
   providers: [
     ApiService,
+    DataService,
     SelectionService
   ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
