5b268781e9f0ddea01e0d6d62d5ef6d588168856
Letty <git@letty.io>
2017-10-19 18:46:37 +0200

    add names to authors

--- server.py
+++ server.py
@@ -122,7 +122,9 @@ def filter_by_topic_result_person():
     person_result = {'data': None, 'error': None}
 
     with connection.cursor() as cursor:
-        sql = 'select * from dnb_author_topic where t_id = %s order by count desc limit 20'
+        sql = 'select a.a_id, ac.name, ac.lastname,a.count '\
+            'from dnb_author_topic a inner join dnb_author_count ac '\
+            'on a.a_id=ac.id where a.t_id =%s order by count desc limit 20'
         try:
             cursor.execute(sql, (topic_id))
         except:
