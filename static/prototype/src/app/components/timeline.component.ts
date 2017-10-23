25a3b8f48eb62d30ac13a0d1f4723697a8988dd8
Letty <git@letty.io>
2017-10-23 15:44:33 +0200

    add brushing event to area chart and transform x0 and x1 of brushing rectangle to years add selection and dataservice to constructor after brushing event is finished: add min and max year to selection and make filter request in dataservice

--- static/prototype/src/app/components/timeline.component.ts
+++ static/prototype/src/app/components/timeline.component.ts
@@ -3,6 +3,9 @@ import {ApiService} from '../services/api.service';
 import * as d3 from 'd3';
 
 import {IYear} from "../app.interfaces";
+import {SelectionService} from "../services/selection.service";
+import {getClassMembers} from "@angular/compiler-cli/src/diagnostics/typescript_symbols";
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'timeline',
@@ -12,7 +15,7 @@ import {IYear} from "../app.interfaces";
 export class TimelineComponent {
   private years: Array<IYear>;
 
-  constructor(private api: ApiService, private myElement: ElementRef, private ren: Renderer2) {
+  constructor(private api: ApiService, private selection: SelectionService, private dataService: DataService) {
 
   }
 
@@ -42,14 +45,9 @@ export class TimelineComponent {
 
     let margin = {top: 20, bottom: 20, left: 50, right: 20};
 
-    let height = 200 - margin.top - margin.bottom,
-      width = 1350 - margin.left - margin.right;
-    let svg = d3.select('#viz').append('svg')
-      .attr('height', height + margin.top + margin.bottom)
-      .attr('width', width + margin.left + margin.right)
-      .attr('class', 'year-area-svg')
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    let height = 200 - margin.top - margin.bottom;
+    let width = 1350 - margin.left - margin.right;
+
 
     let x = d3.scaleTime()
       .rangeRound([0, width]);
@@ -61,29 +59,47 @@ export class TimelineComponent {
 
     let brush = d3.brushX()
       .extent([[0, 0], [width, height]])
-      .on("brush end", this.brushed);
+      .on("brush end", () => {
+        if (d3.event.type === 'end' && (d3.event.selection[0] != 0 && d3.event.selection[1] != width)) {
+          this.selection.setYear(x.invert(d3.event.selection[0]).getFullYear(),
+            x.invert(d3.event.selection[1]).getFullYear());
+          this.dataService.setFilter();
+        }
+      });
 
     let area = d3.area<IYear>()
-      .x(d => { return x(new Date(d.year,1,1));})
+      .x(d => {
+        return x(new Date(d.year, 1, 1));
+      })
       .y0(height)
-      .y1(d => { return y(d.count);});
+      .y1(d => {
+        return y(d.count);
+      });
 
-      // ------
-      x.domain(d3.extent(this.years, function (d) {
-        return new Date(d.year,1,1);
-      }));
-      y.domain([0, d3.max(this.years, function (d) {
-        return d.count;
-      })]);
 
-      svg.append("path")
-        .datum(this.years)
-        .attr('class', 'area-path')
-        .attr("d", area)
-        .on('mousemove', function (d) {
-          // extraxtion des jahres + infos
-          //console.log(x.invert(d3.mouse(this)[0]).getFullYear(), year[new Date(x.invert(d3.mouse(this)[0]).getFullYear(),1,1)]);
-        });
+    let svg = d3.select('#viz').append('svg')
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('width', width + margin.left + margin.right)
+      .attr('class', 'year-area-svg')
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    // ------
+    x.domain(d3.extent(this.years, function (d) {
+      return new Date(d.year, 1, 1);
+    }));
+    y.domain([0, d3.max(this.years, function (d) {
+      return d.count;
+    })]);
+
+    svg.append("path")
+      .datum(this.years)
+      .attr('class', 'area-path')
+      .attr("d", area)
+      .on('mousemove', function (d) {
+        // extraxtion des jahres + infos
+        //console.log(x.invert(d3.mouse(this)[0]).getFullYear(), year[new Date(x.invert(d3.mouse(this)[0]).getFullYear(),1,1)]);
+      });
 
     svg.append("g")
       .attr("class", "brush")
@@ -95,26 +111,20 @@ export class TimelineComponent {
     d3.select('.brush .handle')
       .style('display', 'none');
 
-      svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+    svg.append("g")
+      .attr('class', 'y-axis')
+      .call(d3.axisLeft(y)
+        .tickSize(-width))
+      .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Veröffentlichungen");
 
-      svg.append("g")
-        .attr('class', 'y-axis')
-        .call(d3.axisLeft(y)
-          .tickSize(-width))
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Veröffentlichungen");
   }
-
-  brushed():void{
-    console.log('brush event');
-  }
-
-
 }
