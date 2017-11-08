5b2ce13916a344a57aafdf949ef8dac5df19e7a4
Fidel Thomet <fidelthomet@gmail.com>
2017-11-08 14:39:11 +0100

    [WIP] topic-detail.component: new packing

--- static/prototype/src/app/components/topic-detail/topic-detail.component.ts
+++ static/prototype/src/app/components/topic-detail/topic-detail.component.ts
@@ -40,7 +40,7 @@ export class TopicDetailComponent implements OnInit, OnChanges {
   onResize(event) {
     console.log('resize');
     this.width = this.svg.nativeElement.clientWidth;
-    this.translate = `translate(${this.width / 2} ${this.height / 2})`;
+    // this.translate = `translate(${this.width / 2} ${this.height / 2})`;
     this.simulate(this.nodes);
   }
 
@@ -50,7 +50,7 @@ export class TopicDetailComponent implements OnInit, OnChanges {
     this.width = this.svg.nativeElement.clientWidth;
     this.height = window.innerHeight - 288;
 
-    this.translate = `translate(${this.width / 2} ${this.height / 2})`;
+    // this.translate = `translate(${this.width / 2} ${this.height / 2})`;
 
     this.simulation = this.getSimulation();
 
@@ -63,17 +63,106 @@ export class TopicDetailComponent implements OnInit, OnChanges {
         let yOffset = 0;
         group.forEach(d => {
 
-          d.height = Math.max(d.percentage * 2, 32);
-          d.x = i * this.width / 5;
+          d.height = Math.max(d.percentage * 2, 0);
+          d.x = i * this.width / 5 + this.width / 10;
           d.y = yOffset + d.height / 2;
           yOffset += d.height;
         });
         yOffset = 0;
       });
 
-      const nodes = _.cloneDeep(_values.reduce(function(a, b) {
+      let nodes = _values.reduce(function(a, b) {
         return a.concat(b);
-      }, []));
+      }, []);
+
+      nodes = _.sortBy(nodes, 'count');
+
+      if (nodes.length === 0) {
+        return;
+      }
+
+      const height = 300;
+      let remainingWidth = this.width;
+      let remainingCount = nodes.map(n => n.count).reduce((a, b) => a + b);
+      const packs = [{
+        width: 0,
+        count: 0,
+        nodes: []
+      }];
+
+      const minHeight = 32 / height;
+
+      nodes.forEach((node, i) => {
+        let pack = packs[packs.length - 1];
+        const packCount = pack.count + node.count;
+
+        let hasSpace = true;
+
+        pack.nodes.forEach(pNode => {
+          if (pNode.count / packCount < minHeight) {
+            hasSpace = false;
+          }
+        });
+
+        let packWidth = pack.count / remainingCount * remainingWidth;
+
+        if (packWidth > this.width * 0.2) {
+          hasSpace = false;
+        }
+
+        // if (pack.nodes.length > 5) {
+        //   hasSpace = false;
+        // }
+
+        if (hasSpace) {
+          pack.count = packCount;
+          pack.nodes.push(node);
+        }
+
+        if (!hasSpace) {
+          pack.width = packWidth;
+          remainingCount -= pack.count;
+          remainingWidth -= pack.width;
+
+          let yOffset = 0;
+
+          pack.nodes.forEach(pNode => {
+            pNode.width = packWidth;
+            pNode.height = (pNode.count / pack.count * height);
+            pNode.x = remainingWidth + pNode.width / 2;
+            pNode.y = yOffset + pNode.height / 2;
+            yOffset += pNode.height;
+          });
+
+          packs.push({
+            width: 0,
+            count: node.count,
+            nodes: [node]
+          });
+        }
+
+        if (i >= nodes.length - 1) {
+          pack = packs[packs.length - 1];
+
+          packWidth = pack.count / remainingCount * remainingWidth;
+          console.log(packWidth);
+          let yOffset = 0;
+
+          remainingWidth -= packWidth;
+
+          console.log(remainingWidth);
+
+          pack.nodes.forEach(pNode => {
+            pNode.width = packWidth;
+            pNode.height = (pNode.count / pack.count * height);
+            pNode.x = remainingWidth + pNode.width / 2;
+            pNode.y = yOffset + pNode.height / 2;
+            yOffset += pNode.height;
+          });
+        }
+      });
+
+      console.log(packs);
 
       this.nodes = this.nodes.filter(oldNode =>
         nodes.find(newNode => newNode.id === oldNode.id)
@@ -96,7 +185,8 @@ export class TopicDetailComponent implements OnInit, OnChanges {
       .force('link', d3.forceLink()
         .id(function (d: ITopic) { return `${d.id}`; })
         .strength(d => {
-          return (d as any).value * 0.01;
+          // return 0;
+          return (d as any).value * 0.001;
         })
         // .distance(200)
       )
@@ -113,14 +203,12 @@ export class TopicDetailComponent implements OnInit, OnChanges {
       return;
     }
 
-    console.log(this.nodes);
-
     this.links = this.randomLinks();
 
     const collisionForce = rectCollide();
 
     collisionForce.size((d) => {
-      return [this.width / 5 + 20, d.height + 20];
+      return [d.width + 20, d.height + 20];
     });
 
     collisionForce.iterations(1);
@@ -129,10 +217,10 @@ export class TopicDetailComponent implements OnInit, OnChanges {
 
     this.simulation
       .nodes(this.nodes)
+      // ;
       .on('tick', () => {this.ticked(); })
-      // .force('cf', (a) => this.customForce(a));
-      .force('collision', collisionForce)
-      .force('center', d3.forceCenter(0, 0));
+      .force('collision', collisionForce);
+      // .force('center', d3.forceCenter(this.width / 2, this.height / 2));
 
     this.simulation.force('link').links(this.links);
 
@@ -141,11 +229,11 @@ export class TopicDetailComponent implements OnInit, OnChanges {
 
   ticked(): void {
     this.nodes.forEach((n, i) => {
-      n.x = Math.max(n.x, (-this.width + this.width / 5) / 2);
-      n.x = Math.min(n.x, (this.width - this.width / 5) / 2);
+      n.x = Math.max(n.x, n.width * 0.5);
+      n.x = Math.min(n.x, this.width - n.width * 0.5);
 
-      n.y = Math.max(n.y, (-this.height + n.height) / 2);
-      n.y = Math.min(n.y, (this.height - n.height) / 2);
+      n.y = Math.max(n.y, n.height * 0.5);
+      n.y = Math.min(n.y, this.height - n.height * 0.5);
 
     });
 
@@ -157,10 +245,10 @@ export class TopicDetailComponent implements OnInit, OnChanges {
 
       const dir = [dist[0] > 0 ? 1 : -1, dist[1] > 0 ? 1 : -1];
 
-      const p1Corner = [link.source.x + (this.width / 10) * dir[0],
+      const p1Corner = [link.source.x + (link.source.width / 2) * dir[0],
         link.source.y + (link.source.height / 2) * dir[1]];
 
-      const p2Corner = [link.target.x + (this.width / 10) * -dir[0],
+      const p2Corner = [link.target.x + (link.target.width / 2) * -dir[0],
         link.target.y + (link.target.height / 2) * -dir[1]];
 
       const cDist = [p2Corner[0] - p1Corner[0], p2Corner[1] - p1Corner[1]];
@@ -195,12 +283,12 @@ export class TopicDetailComponent implements OnInit, OnChanges {
       }
 
       const s = {
-        x: link.source.x + (this.width / 10) * anchor[0],
+        x: link.source.x + (link.source.width / 10) * anchor[0],
         y: link.source.y + (link.source.height / 2) * anchor[1]
       };
 
       const t = {
-        x: link.target.x + (this.width / 10) * -anchor[0],
+        x: link.target.x + (link.target.width / 10) * -anchor[0],
         y: link.target.y + (link.target.height / 2) * -anchor[1]
       };
 
@@ -215,8 +303,9 @@ export class TopicDetailComponent implements OnInit, OnChanges {
     });
   }
 
-  getTranslate(n) {
-    const transform = `translate(${n.x - this.width / 10}px, ${n.y - n.height / 2}px)`;
+  getTranslate(n, isSVG) {
+    if (isSVG) { return `translate(${n.x - n.width / 2} ${n.y - n.height / 2})`; }
+    const transform = `translate(${n.x - n.width / 2}px, ${n.y - n.height / 2}px)`;
     return this.sanitizer.bypassSecurityTrustStyle(transform);
   }
 
@@ -236,7 +325,7 @@ export class TopicDetailComponent implements OnInit, OnChanges {
 
   randomLinks() {
     if (this.nodes == null) { return; }
-    return '.'.repeat(40).split('').map(d => {
+    return '.'.repeat(20).split('').map(d => {
       return {
         source: this.nodes[Math.floor(Math.random() * this.nodes.length)].id,
         target: this.nodes[Math.floor(Math.random() * this.nodes.length)].id,
