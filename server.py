e1c99a7eb951d875aed495d270b1e2957269f22f
Letty <git@letty.io>
2017-11-14 13:45:36 +0100

    clean server

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import json
 from datetime import datetime, date
 import pymysql.cursors
 import itertools
+import utils
 
 app = Flask(__name__, static_url_path='')
 CORS(app)  # remove for production
@@ -16,95 +17,6 @@ connection = pymysql.connect(host='127.0.0.1',
                              cursorclass=pymysql.cursors.DictCursor)
 
 
-def avoidSmallPercentage(values, threshold):
-    missing_percentage = 0
-    percentage_above_threshold = 0
-
-    # Find out how much percentage is missing and how much is above the
-    # threshold
-    for value in values:
-        if (value['percentage'] < (threshold)):
-            missing_percentage += threshold - value['percentage']
-        else:
-            percentage_above_threshold += value['percentage']
-
-    # This method is not completly clean. The sum of the resulting percentages
-    # are above 100 if items are higher than the threshold but would fall
-    # below if subtracted.
-    if (missing_percentage > 0):
-        for value in values:
-            # Calculate the needed subtraction
-            value_subtraction = value['percentage'] / \
-                percentage_above_threshold * missing_percentage
-            # Check if item would be below threshold after subtraction
-            if (value['percentage'] - value_subtraction < (threshold)):
-                value['percentage'] = (threshold)
-            else:
-                value['percentage'] = value['percentage'] - value_subtraction
-
-    return values
-
-
-def getTopicsPercentage(topics):
-    number_of_bins = 5  # The number of columns the visualization has
-    threshold_percentage = 10
-
-    # Calculate the percentage each topic has
-    total = sum(topic['count'] for topic in topics)
-    for topic in topics:
-        topic['percentage'] = topic['count'] * 100 / total
-
-    # Recalculate to have no values below threshold
-    topics = avoidSmallPercentage(
-        topics, threshold_percentage / number_of_bins)
-
-    # The percentage each column should hold
-    percentage_per_bin = 100 / number_of_bins
-    bins = [[] for _ in range(number_of_bins)]  # Create arrays for each column
-
-    # Distribute the topics to the bins
-    for bin in bins:
-        for topic in topics:
-            if (sum(topic['percentage'] for topic in bin) < percentage_per_bin):
-                bin.append(topic)
-                topics.remove(topic)
-
-    # Sort bins to have the one least filled at front
-    bins.sort(key=lambda bin: sum(topic['percentage']
-                                  for topic in bin), reverse=True)
-
-    # Distribute the remaining topics to the columns
-    current_bin = 0
-    for topic in topics:
-        bins[current_bin].append(topic)
-        current_bin += 1
-        if current_bin == number_of_bins:
-            current_bin = 0
-
-    # Filter empty columns
-    bins = list(filter(lambda bin: len(bin) != 0, bins))
-
-    # Calculate the percentage each topic has of its column
-    for bin in bins:
-        total = sum(topic['percentage'] for topic in bin)
-        for topic in bin:
-            topic['percentage'] = topic['percentage'] * 100 / total
-
-        # Recalculate again to have no values below threshold
-        bin = avoidSmallPercentage(bin, threshold_percentage)
-
-        # Sort items to have biggest at top
-        bin.sort(key=lambda topic: topic['percentage'], reverse=True)
-
-    # Sort bins to have the one with the highest first value first
-    bins.sort(key=lambda bin: bin[0]['percentage'], reverse=True)
-
-    # Sort bins to have the one with the least topics first
-    # bins.sort(key = lambda bin: len(bin))
-
-    return bins
-
-
 @app.route('/')
 def root():
     return app.send_static_file('dist/index.html')
@@ -112,8 +24,8 @@ def root():
 
 @app.route('/getTopTopics')
 def get_top_topics():
-    result = get_default_topics()
-    return jsonify(getTopicsPercentage(result))
+    result = utils.get_default_topics(connection)
+    return jsonify(utils.getTopicsPercentage(result))
 
 
 @app.route('/getTopPeople')
@@ -178,7 +90,7 @@ def filter_by_person_result_topic():
         except:
             topic_result['error'] = str(sys.exc_info()[0])
         else:
-            topic_result['data'] = getTopicsPercentage(cursor.fetchall())
+            topic_result['data'] = utils.getTopicsPercentage(cursor.fetchall())
     uptime = str(datetime.now() - startTime)
     print('uptime: %s', (uptime))
     return jsonify(topic_result)
@@ -326,7 +238,7 @@ def filter_by_year_result_topic():
         except:
             topic_result['error'] = str(sys.exc_info()[0])
         else:
-            topic_result['data'] = getTopicsPercentage(cursor.fetchall())
+            topic_result['data'] = utils.getTopicsPercentage(cursor.fetchall())
     return jsonify(topic_result)
 
 
@@ -405,7 +317,7 @@ def filter_by_year_person_result_topic():
                 t.append({'id': key, 'keyword': topics[key][
                          'keyword'], 'count': topics[key]['count']})
 
-        topic_result['data'] = getTopicsPercentage(t)
+        topic_result['data'] = utils.getTopicsPercentage(t)
     return jsonify(topic_result)
 
 
@@ -652,46 +564,10 @@ def search_for_topic():
 @app.route('/getTopTopicConnections')
 def get_top_topic_connections():
     network_result = {'data': []}
-    result = get_default_topics()
-    network_result['data'] = combine_topics(result)
+    result = utils.get_default_topics(connection)
+    network_result['data'] = utils.combine_topics(result, connection)
 
     return jsonify(network_result)
-
-
-def get_default_topics():
-    result = []
-    with connection.cursor() as cursor:
-        sql = 'select * from dnb_topic_count order by count DESC limit 20'
-        cursor.execute(sql)
-        result = cursor.fetchall()
-    return result
-
-
-def combine_topics(topics):
-    result = []
-    topic_comb = list(itertools.combinations(topics, 2))
-
-    with connection.cursor() as cursor:
-        for t in topic_comb:
-            r = {'source': t[0]['id'], 'target': t[1]['id'], 'strength': 0}
-
-            sql = 'select count from dnb_topic_topic where t_id1=%s and t_id2=%s'
-            cursor.execute(sql, (t[0]['id'], t[1]['id']))
-            f = cursor.fetchone()
-            if f != None:
-                r['strength'] += f['count']
-
-            cursor.execute(sql, (t[1]['id'], t[0]['id']))
-            f = cursor.fetchone()
-            if f != None:
-                r['strength'] += f['count']
-
-            result.append(r)
-    return result
-
-
-def seq_iter(obj):
-    return obj if isinstance(obj, dict) else range(len(obj))
 
 # for aws foo
 # app.run(host='0.0.0.0', port=80)
