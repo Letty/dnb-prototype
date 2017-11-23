8afaf733084a42dde4a586f386d9de85bce711e9
Jonas <jonasparnow@gmail.com>
2017-11-23 13:43:11 +0100

    Piwik Action Tracking

---
+++ static/prototype/src/app/services/piwikTracking.ts
@@ -0,0 +1,10 @@
+const enable_trackPiwik = true;
+declare var _paq:any; // Because _pag is declared outside of TypeScript
+
+export function trackPiwik(event, name, _value = undefined) {
+  if (enable_trackPiwik && typeof _paq !== undefined) {
+    const value = _value instanceof Array ? _value.join('-') : _value;
+    console.log('TrackPiwik', event, name, value)
+    _paq.push(['trackEvent', event, name, value]);
+  }
+}
