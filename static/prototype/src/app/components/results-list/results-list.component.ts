1178f9641a78872d64a119392dd611f20909f4d4
Fidel Thomet <fidelthomet@gmail.com>
2017-11-23 17:58:06 +0100

    [Fix] results-list.component: scrolling

--- static/prototype/src/app/components/results-list/results-list.component.ts
+++ static/prototype/src/app/components/results-list/results-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, NgModule} from '@angular/core';
+import {Component, OnInit, NgModule, ViewChild} from '@angular/core';
 import {ApiService} from '../../services/api.service';
 
 import {IYear, IItem} from '../../app.interfaces';
@@ -21,6 +21,8 @@ import { MasonryModule } from 'angular2-masonry';
 })
 
 export class ResultsListComponent implements OnInit {
+  @ViewChild('resultList') resultList;
+
   public items: Observable<IItem[]>;
   public item: any = null;
   public itemTitle: string = null;
@@ -40,6 +42,7 @@ export class ResultsListComponent implements OnInit {
     this.items = this.dataService.items;
     this.dataService.items.subscribe(value => {
       this.loadingData = false;
+      this.resultList.nativeElement.scrollTop = 0;
     });
 
   }
