09d03985834f52305d3fdaaa8e593ffb0d7e354b
Jonas <jonasparnow@gmail.com>
2017-11-29 13:44:27 +0100

    Get PiwikID fixed

--- static/prototype/src/app/components/dnb-header/dnb-header.component.ts
+++ static/prototype/src/app/components/dnb-header/dnb-header.component.ts
@@ -3,7 +3,6 @@ import {Observable} from 'rxjs';
 
 const enable_trackPiwik = true;
 declare var _paq: any; // Because _pag is declared outside of TypeScript
-declare const Piwik: any;
 
 @Component({
   selector: 'dnb-header',
@@ -12,46 +11,23 @@ declare const Piwik: any;
     })
 
 export class DnbHeaderComponent {
-	private piwikVisitorId: string = '';
+	public piwikVisitorId = '';
 
   constructor() {
   }
 
-  ngOnInit() {
-    console.log(Piwik.getTracker().getVisitorId());
+  ngOnInit(): void {
   	if (enable_trackPiwik && typeof _paq !== undefined) {
 	    let visitor_id;
 	    let that = this;
 			_paq.push([ function() {
 				visitor_id = this.getVisitorId();
-				console.log('Message inside', visitor_id);
-				// return visitor_id;
 				that.setPiwikID(visitor_id);
-			}.bind(that.setPiwikID)]);
-			console.log('Message after', visitor_id);
-			// visitor_id.subscribe(visitor_id => {
-	  //     this.piwikVisitorId = visitor_id;
-	  //   });
+			}]);
 	  }
   }
 
-  setPiwikID(id) {
-  	console.log('you')
+  setPiwikID(id: string): void {
   	this.piwikVisitorId = id;
   }
-
-  // _getPiwikID() {
-  // 	if (this.piwikVisitorId === '') {
-  // 		// this.piwikVisitorId = getPiwikID();
-  // 		if (enable_trackPiwik && typeof _paq !== undefined) {
-		//     let visitor_id;
-		// 		_paq.push([ function() {
-		// 			this.piwikVisitorId = this.getVisitorId();
-		// 			console.log('Message inside', visitor_id);
-		// 			// return this.piwikVisitorId;
-		// 		}]);
-		// 		console.log('Message after', visitor_id);
-		//   }
-  // 	}
-  // }
 }
